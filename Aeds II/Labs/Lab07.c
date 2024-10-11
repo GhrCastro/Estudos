70feff706421fa6a5560abd60d7ae0a883dff7ee
guguh1612 <143287983+guguh1612@users.noreply.github.com>
2024-10-10 22:09:55 -0300

    Understandable comments

--- Aeds II/Labs/Lab07.c
+++ Aeds II/Labs/Lab07.c
@@ -11,18 +11,32 @@ int contarUltrapassagens(int* largada, int* chegada, int n) {
 
     // Preenche o array com as posições da largada
     for (int i = 0; i < n; i++) {
-        pos_largada[largada[i]] = i;
+        pos_largada[largada[i]] = i; 
     }
+    /*
+    i=0, largada[i] = largada[0]=>1
+    pos_largada[largada[i]] = pos_largada[largada[0]] = pos_largada[1]=>0
+    */
 
     //Conta as ultrapassagens comparando as posições
-    for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
+    for (int i = 0; i < n; i++) {                                                                              
+        for (int j = i + 1; j < n; j++) {                                                                     
             // Se um corredor que estava atrás na largada chegou antes, houve ultrapassagem
-            if (pos_largada[chegada[i]] > pos_largada[chegada[j]]) {
+            if (pos_largada[chegada[i]] > pos_largada[chegada[j]]) {       i=0; j=0+1; j->chegada[1] = 2; i->chegada[0] = 3;  pos_largada[3] = 4;  pos_largada[2] = 1;       
                 ultrapassagens++;
             }
         }
     }
+
+    /*
+    largda[n] 0-n largada[0]=>1
+    chegada[n] 0-n chegada[0]=>3       
+                                  
+    i=0, chegada[i] = chegada[0] =>3
+    j=0+1, j=1, chegada[j] = chegada[1]=>2
+
+    pos_largada[chegada[i]] = pos_largada[chegada[0]] = pos_largada[3] => 
+    */
 
     return ultrapassagens;
 }
