400f8425805a2889d2d8a8ca117dd4c1b05154d4
Gustavo Henrique Rodrigues De Castro <143287983+GhrCastro@users.noreply.github.com>
2025-08-31 00:44:21 -0300

    I.A Folder creation

---
+++ I.A/questao1.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+from sklearn.tree import DecisionTreeClassifier
+
+base = pd.read_csv('C:\Users\gugsh\Repos\I.A\restaurante (1).csv', ';')
+
+
+
+
+
+cliente_nenhum = 0
+cliente_algum = 1
+cliente_cheio = 2
